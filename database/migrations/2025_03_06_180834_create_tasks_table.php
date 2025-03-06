72be709173d134eae681d03f93bdc61d7e1dcdb5
Jorge Sánchez López <jorgeraton70@gmail.com>
2025-03-06 19:48:56 +0100

    feat: add migrations
    first phase to create an Asana BD

---
+++ database/migrations/2025_03_06_180834_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('tasks', function (Blueprint $table) {
+			$table->id();
+			$table->string('title');
+			$table->text('description')->nullable();
+			$table->foreignId('project_id')->references('id')->on('projects')->onDelete('cascade');
+			$table->foreignId('assigned_to')->references('id')->on('users')->onDelete('cascade');
+			$table->foreignId('section_id')->references('id')->on('sections')->onDelete('cascade');
+			$table->jsonb('attachment')->nullable();
+			$table->string('status')->default('pending');
+			$table->timestamp('due_date')->nullable();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('tasks');
+	}
+};
