e90916dd282887d63cdf2cefd46c646b54023a95
Jorge Sánchez López <jorgeraton70@gmail.com>
2025-03-09 19:31:17 +0100

    feat(migrations,factory,models): created project factory and seeder. fill first implementation of models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Comment extends Model
 {
-    //
+	use HasFactory;
+
+	protected $fillable = [
+		'content',
+		'created_at'
+	];
+
+
+	/* comentarios de una tarea */
+	public function tasks(): BelongsToMany
+	{
+		return $this->belongsToMany(Task::class);
+	}
+
+	/* usuario a comentado en una tarea */
+	public function users(): BelongsToMany
+	{
+		return $this->belongsToMany(User::class);
+	}
+
+
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,9 +2,44 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
-    //
+	use HasFactory;
+
+	protected $fillable = [
+		'name',
+		'description',
+		'created_at'
+	];
+
+	/* coloaboradores de un proyecto */
+	public function users(): BelongsToMany
+	{
+		return $this->belongsToMany(User::class, 'project_members')
+			->withPivot(['id', 'role']);
+	}
+
+	/* las tareas de un proyecto */
+	public function tasks(): HasMany
+	{
+		return $this->hasMany(Task::class);
+	}
+
+	/* tareas para una seccion del proyecto */
+	public function sections(): BelongsToMany
+	{
+		return $this->belongsToMany(Task::class, 'sections')
+			->withPivot(['id', 'name']);
+	}
+
+	/* filtros de un proyecto */
+	public function filters(): HasMany
+	{
+		return $this->hasMany(Filter::class);
+	}
 }
