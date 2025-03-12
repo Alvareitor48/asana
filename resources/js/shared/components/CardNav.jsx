ef6886f07f2815f990ed824e5c8600c5dec66708
Alvareitor48 <alvaroelguaperas499@gmail.com>
2025-03-12 13:33:37 +0100

    feat(projects): how view added and its controller refactored too

--- resources/js/shared/components/CardNav.jsx
+++ resources/js/shared/components/CardNav.jsx
@@ -1,7 +1,9 @@
-const CardNav = ({ name, children, className = '' }) => {
+const CardNav = ({ name, children, className = '', onClick = null }) => {
   return (
     <button
       className={`hover:bg-active-nav flex h-fit w-full items-center justify-start gap-4 rounded-md px-3 py-2 text-white ${className}`}
+      type="button"
+      onClick={onClick && onClick}
     >
       {children}
       <span className="text-lg">{name}</span>
