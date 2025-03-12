68d138f5e5adeaa3d1b10ecc4e6032522bb39cb8
Alvareitor48 <alvaroelguaperas499@gmail.com>
2025-03-12 18:02:47 +0100

    feat(factory, seeder) comment factory and seeder created and updated project seeder too

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
 		$this->call([
 			UserSeeder::class,
 			ProjectSeeder::class,
-			TaskSeeder::class
+			TaskSeeder::class,
+			CommentSeeder::class,
 		]);
 	}
 }
