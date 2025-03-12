4b4c3c8799d22fadaaeb4e1904592ee8656047fa
Alvareitor48 <alvaroelguaperas499@gmail.com>
2025-03-12 11:26:11 +0100

    fix(migrations): trigger updated to support mysql

--- database/migrations/2025_03_09_140529_create_user_project_trigger.php
+++ database/migrations/2025_03_09_140529_create_user_project_trigger.php
@@ -5,49 +5,32 @@ use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
-	/**
-	 * Run the migrations.
-	 */
 	public function up(): void
 	{
-		DB::unprepared('
-            CREATE OR REPLACE FUNCTION create_project_after_user_insert()
-            RETURNS TRIGGER AS $$
-            DECLARE
-                new_project_id INT; -- Usar INT para el ID del proyecto
-            BEGIN
-                -- Crear un nuevo proyecto y obtener su ID
-                INSERT INTO projects (name, description, created_at)
-                VALUES (
-                    CONCAT(\'My Tasks \', NEW.name),
-                    CONCAT(\'Personal tasks for user \', NEW.name),
-                    NOW()
-                )
-                RETURNING id INTO new_project_id; -- Capturar el ID generado
-
-                -- Insertar al usuario en project_members con el rol de "owner"
-                INSERT INTO project_members (project_id, user_id, role)
-                VALUES (new_project_id, NEW.id, \'owner\');
-
-                RETURN NEW;
-            END;
-            $$ LANGUAGE plpgsql;
-        ');
-
-		DB::unprepared('
+		DB::unprepared("
             CREATE TRIGGER create_project_after_user_insert
             AFTER INSERT ON users
             FOR EACH ROW
-            EXECUTE FUNCTION create_project_after_user_insert();
-        ');
+            BEGIN
+                DECLARE new_project_id INT;
+
+                INSERT INTO projects (name, description, created_at)
+                VALUES (
+                    CONCAT('My Tasks ', NEW.name),
+                    CONCAT('Personal tasks for user ', NEW.name),
+                    NOW()
+                );
+
+                SET new_project_id = LAST_INSERT_ID();
+
+                INSERT INTO project_members (project_id, user_id, role)
+                VALUES (new_project_id, NEW.id, 'owner');
+            END
+        ");
 	}
 
-	/**
-	 * Reverse the migrations.
-	 */
 	public function down(): void
 	{
-		DB::unprepared('DROP TRIGGER IF EXISTS create_project_after_user_insert ON users');
-		DB::unprepared('DROP FUNCTION IF EXISTS create_project_after_user_insert');
+		DB::unprepared("DROP TRIGGER IF EXISTS create_project_after_user_insert");
 	}
 };
