05a055fa4c925a92d98e22a2b2ee04ca70501da0
Jorge Sánchez López <jorgeraton70@gmail.com>
2025-03-20 19:43:37 +0100

    feat(project): add ProjectModal component for project creation

---
+++ resources/js/shared/components/ProjectModal.jsx
@@ -0,0 +1,64 @@
+import InputLabel from '@/modules/auth/components/InputLabel'
+import TextInput from '@/modules/auth/components/TextInput'
+import Modal from './Modal'
+
+export const PorjectModal = ({
+  isModalOpen,
+  processing,
+  data,
+  errors,
+  closeModal,
+  handleSubmit,
+  setData,
+}) => {
+  return (
+    <Modal isOpen={isModalOpen} onClose={closeModal}>
+      <div className="text-white flex flex-col justify-start items-start w-full">
+        <h2 className="self-center text-3xl">Nuevo Proyecto</h2>
+        {/* nombre tarea */}
+        <form onSubmit={handleSubmit} className="w-full h-fit">
+          <div className="flex justify-between items-center gap-10 my-5 w-full">
+            <InputLabel
+              className="text-white w-[100px]"
+              htmlFor="responsable"
+              value="Nombre del proyecto"
+            />
+            <TextInput
+              id="name"
+              type="text"
+              name="name"
+              value={data.name}
+              onChange={(e) => setData('name', e.target.value)}
+              className="name mt-1 block w-full text-white bg-slate-400/30"
+            />
+          </div>
+          {errors.name && <p className="text-red-500 text-center">{errors.name}</p>}
+          <div className="flex justify-between items-center gap-10 my-5 w-full">
+            <InputLabel
+              className="text-white w-[100px]"
+              htmlFor="responsable"
+              value="Descripción"
+            />
+            <TextInput
+              id="name"
+              type="text"
+              name="name"
+              value={data.description}
+              onChange={(e) => setData('description', e.target.value)}
+              className="name mt-1 block w-full text-white bg-slate-400/30"
+            />
+          </div>
+          {errors.description && (
+            <p className="text-red-500 text-center mb-3">{errors.description}</p>
+          )}
+          <button
+            disabled={processing}
+            className="mb-2 w-full bg-white text-black rounded-md px-4 py-1 transition-transform duration-200 hover:scale-105 active:scale-95"
+          >
+            {processing ? 'Creando...' : 'Crear'}
+          </button>
+        </form>
+      </div>
+    </Modal>
+  )
+}
--- resources/js/shared/layouts/AuthLayout.jsx
+++ resources/js/shared/layouts/AuthLayout.jsx
@@ -1,5 +1,3 @@
-import InputLabel from '@/modules/auth/components/InputLabel'
-import TextInput from '@/modules/auth/components/TextInput'
 import CardNav from '@/shared/components/CardNav'
 import Header from '@/shared/components/Header'
 import NavBar from '@/shared/components/NavBar'
@@ -9,7 +7,7 @@ import Pen from '@/shared/icons/Pen'
 import { router, useForm, usePage } from '@inertiajs/react'
 import { AnimatePresence, motion } from 'motion/react'
 import { useRef, useState } from 'react'
-import Modal from '../components/Modal'
+import { PorjectModal } from '../components/ProjectModal'
 import Add from '../icons/Add'
 import ArrowDown from '../icons/ArrowDown'
 
@@ -142,54 +140,15 @@ export default function AuthLayout({ children }) {
         </AnimatePresence>
       </div>
 
-      <Modal isOpen={isModalOpen} onClose={closeModal}>
-        <div className="text-white flex flex-col justify-start items-start w-full">
-          <h2 className="self-center text-3xl">Nuevo Proyecto</h2>
-          {/* nombre tarea */}
-          <form onSubmit={handleSubmit} className="w-full h-fit">
-            <div className="flex justify-between items-center gap-10 my-5 w-full">
-              <InputLabel
-                className="text-white w-[100px]"
-                htmlFor="responsable"
-                value="Nombre del proyecto"
-              />
-              <TextInput
-                id="name"
-                type="text"
-                name="name"
-                value={data.name}
-                onChange={(e) => setData('name', e.target.value)}
-                className="name mt-1 block w-full text-white bg-slate-400/30"
-              />
-            </div>
-            {errors.name && <p className="text-red-500 text-center">{errors.name}</p>}
-            <div className="flex justify-between items-center gap-10 my-5 w-full">
-              <InputLabel
-                className="text-white w-[100px]"
-                htmlFor="responsable"
-                value="Descripción"
-              />
-              <TextInput
-                id="name"
-                type="text"
-                name="name"
-                value={data.description}
-                onChange={(e) => setData('description', e.target.value)}
-                className="name mt-1 block w-full text-white bg-slate-400/30"
-              />
-            </div>
-            {errors.description && (
-              <p className="text-red-500 text-center mb-3">{errors.description}</p>
-            )}
-            <button
-              disabled={processing}
-              className="mb-2 w-full bg-white text-black rounded-md px-4 py-1 transition-transform duration-200 hover:scale-105 active:scale-95"
-            >
-              {processing ? 'Creando...' : 'Crear'}
-            </button>
-          </form>
-        </div>
-      </Modal>
+      <PorjectModal
+        handleSubmit={handleSubmit}
+        setData={setData}
+        processing={processing}
+        closeModal={closeModal}
+        isModalOpen={isModalOpen}
+        data={data}
+        errors={errors}
+      />
     </>
   )
 }
