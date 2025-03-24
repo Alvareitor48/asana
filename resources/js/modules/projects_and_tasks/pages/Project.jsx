ae92521e39e100d4b2eff38b95ccb1cfa75f0952
Jorge Sánchez López <jorgeraton70@gmail.com>
2025-03-24 12:21:11 +0100

    feat(project): add edit project modal and functionality

--- resources/js/modules/projects_and_tasks/pages/Project.jsx
+++ resources/js/modules/projects_and_tasks/pages/Project.jsx
@@ -1,5 +1,6 @@
+import { EditProjectModal } from '@/shared/components/EditProjectModal'
 import AuthLayout from '@/shared/layouts/AuthLayout'
-import { router, usePage } from '@inertiajs/react'
+import { router, useForm, usePage } from '@inertiajs/react'
 import { useRef, useState } from 'react'
 import { DeleteProjectModal } from '../components/DeleteProjectModal'
 import Tbody from '../components/Tbody'
@@ -18,9 +19,13 @@ export default function Project({ project, sections: sec }) {
   })
 
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isEditProjectModalOpen, setIsEditProjectModalOpen] = useState(false)
   const containerRef = useRef(null)
   const openModal = () => setIsModalOpen(true)
   const closeModal = () => setIsModalOpen(false)
+
+  const openEditProjectModal = () => setIsEditProjectModalOpen(true)
+  const closeEditProjectModal = () => setIsEditProjectModalOpen(false)
 
   const toggleSection = (sectionId) => {
     setCollapsedSections({
@@ -41,6 +46,23 @@ export default function Project({ project, sections: sec }) {
       })
   }
 
+  const { data, setData, processing, errors, reset } = useForm({
+    name: '',
+    description: '',
+    color_icon: '#000',
+  })
+
+  const handleEditProjectSubmit = (e) => {
+    e.preventDefault()
+
+    /*  post(route('project.store'), {
+      onSuccess: () => {
+        reset()
+        setIsModalOpen(false)
+      },
+    }) */
+  }
+
   return (
     <>
       <AuthLayout>
@@ -53,7 +75,13 @@ export default function Project({ project, sections: sec }) {
             <div className="flex items-center gap-3">
               <div className="w-5 h-5 rounded-md" style={{ backgroundColor: project.color_icon }} />
               <h1 className="text-xl font-bold text-white">{project.name}</h1>
-              {is_project_owner && <button onClick={openModal}>❌</button>}
+
+              <button
+                className='<button className="my-2 w-fit bg-blue-900/80 text-white rounded-md px-4 py-1 transition-transform duration-200 hover:scale-105 active:scale-95">'
+                onClick={openEditProjectModal}
+              >
+                Editar
+              </button>
             </div>
             <button className="my-2 w-fit bg-white/60 text-black rounded-md px-4 py-1 transition-transform duration-200 hover:scale-105 active:scale-95">
               Añadir Filtro
@@ -75,6 +103,21 @@ export default function Project({ project, sections: sec }) {
           </div>
         </div>
       </AuthLayout>
+
+      <EditProjectModal
+        isModalOpen={isEditProjectModalOpen}
+        openModal={openEditProjectModal}
+        closeModal={closeEditProjectModal}
+        project={project}
+        handleSubmit={handleEditProjectSubmit}
+        setData={setData}
+        processing={processing}
+        data={data}
+        errors={errors}
+        openDeleteProject={openModal}
+        is_project_owner={is_project_owner}
+      />
+
       {is_project_owner && (
         <DeleteProjectModal
           isModalOpen={isModalOpen}
