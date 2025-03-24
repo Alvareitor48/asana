1c726423fc9522606b318a7a54fb35cdba3b97d3
Alvareitor48 <alvaroelguaperas499@gmail.com>
2025-03-24 12:25:57 +0100

    fix(redirect): fix redirection error when you redirect home

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -16,7 +16,7 @@ class EmailVerificationPromptController extends Controller
 	public function __invoke(Request $request): RedirectResponse|Response
 	{
 		return $request->user()->hasVerifiedEmail()
-			? redirect()->intended(route('home', absolute: false))
+			? Inertia::location(route('home'))
 			: Inertia::render('auth/pages/VerifyEmail', ['status' => session('status')]);
 	}
 }
